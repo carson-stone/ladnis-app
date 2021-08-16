e5339b6c8ea1470267cbe3150a45258196f9c449
Carson Stone <carson.stone123@gmail.com>
2021-08-16 16:40:36 -0400

    added footer

---
+++ frontend/components/Footer.js
@@ -0,0 +1,21 @@
+export default function Footer() {
+	return (
+		<footer className='flex items-center justify-center w-full h-24 border-t space-x-8'>
+			<a
+				href='https://linkedin.com/in/carson-stone'
+				target='_blank'
+				rel='noopener noreferrer'
+			>
+				By Carson Stone
+			</a>
+			<a
+				href='https://github.com/carson-stone/landis-app'
+				target='_blank'
+				rel='noopener noreferrer'
+				className='text-blue'
+			>
+				GitHub
+			</a>
+		</footer>
+	);
+}
