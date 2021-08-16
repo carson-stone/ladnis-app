23dbf73447d0776e7ef37f1c83984175e828a911
Carson Stone <carson.stone123@gmail.com>
2021-08-16 13:21:53 -0400

    changed title and added color theme to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,29 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	mode: 'jit',
+	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				green: {
+					DEFAULT: '#4FEA83',
+				},
+				blue: {
+					DEFAULT: '#445FDC',
+				},
+				purple: {
+					light: '#FEFCFF',
+					DEFAULT: '#223078',
+				},
+				gray: {
+					light: '#FAFAFA',
+					med: '#DCDBE3',
+				},
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
