7fc506ffcfa5125cf71f227d30d2f9d1e2500322
Carson Stone <carson.stone123@gmail.com>
2021-08-17 00:20:51 -0400

    fetch users from backend

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,7 @@
+import axios from 'axios';
+
 export default function Home({ users }) {
-	const TARGET_BALANCE_TO_PURCHASE = 8000.0;
+	const TARGET_BALANCE_TO_PURCHASE = 20000.0;
 	const TARGET_CREDIT_TO_PURCHASE = 800;
 
 	function toFullName(first, last) {
@@ -34,7 +36,6 @@ export default function Home({ users }) {
 
 	function getPurchaseProgressBar(balance, credit) {
 		const progress = calculatePurchaseProgress(balance, credit);
-		console.log('progress', progress);
 		if (progress < 0.1) {
 			return (
 				<div className='absolute bottom-0 left-0 rounded-xl bg-green w-[0%] h-1'></div>
@@ -87,91 +88,87 @@ export default function Home({ users }) {
 	return (
 		<div className='flex flex-col items-center justify-center min-h-screen py-2'>
 			<h1 className='text-6xl font-bold text-blue mb-8'>Landis Accounts</h1>
-			{users.map((user) => {
-				const fullName = toFullName(user.name_first, user.name_last);
-				const formattedBalance = formatDollarAmount(user.balance);
-				const purchaseProgress = getFormattedPurchaseProgress(
-					user.balance,
-					user.credit
-				);
-				const progressBar = getPurchaseProgressBar(user.balance, user.credit);
 
-				return (
-					<div key={user.id} className='min-w-[600px] shadow-md rounded-lg'>
-						<div className='flex justify-between items-center space-x-6 bg-white px-10 py-2'>
-							<div className='flex items-center space-x-7'>
-								<img
-									src={user.picture}
-									alt={fullName}
-									className='rounded-full w-20 h-20'
-								/>
-								<p className='text-xl text-purple font-bold max-w-xs'>
-									{fullName}
-								</p>
-							</div>
-							<div className='flex flex-col items-end'>
-								<a href={`tel:${user.phone}`} className='text-purple underline'>
-									{user.phone}
-								</a>
-								<a
-									href={`mailto:${user.email}`}
-									className='text-purple underline'
-								>
-									{user.email}
-								</a>
-							</div>
-						</div>
-						<div className='flex justify-between px-10 py-12 bg-purple-light'>
-							<div className='flex space-x-8 border-r border-gray pr-16'>
-								<div className='flex flex-col space-y-8'>
-									<p className='text-purple font-bold'>Balance</p>
-									<p className='text-purple font-bold'>Credit</p>
-									<p className='text-purple font-bold'>Purchase Progress</p>
+			<div className='space-y-8'>
+				{users.map((user) => {
+					const fullName = toFullName(user.name_first, user.name_last);
+					const formattedBalance = formatDollarAmount(user.balance);
+					const purchaseProgress = getFormattedPurchaseProgress(
+						user.balance,
+						user.credit
+					);
+					const progressBar = getPurchaseProgressBar(user.balance, user.credit);
+
+					return (
+						<div key={user.id} className='min-w-[600px] shadow-md rounded-lg'>
+							<div className='flex justify-between items-center space-x-6 bg-white px-10 py-2'>
+								<div className='flex items-center space-x-7'>
+									<img
+										src={user.picture}
+										alt={fullName}
+										className='rounded-full w-20 h-20'
+									/>
+									<p className='text-xl text-purple font-bold max-w-xs'>
+										{fullName}
+									</p>
 								</div>
-								<div className='relative flex flex-col items-end space-y-8'>
-									<p className='text-purple'>{formattedBalance}</p>
-									<p className='text-purple'>{user.credit}</p>
-									<p className='text-purple'>{purchaseProgress}</p>
-									<div className='absolute -bottom-2'>
-										<div className='relative w-14 h-1'>
-											<div className='absolute bottom-0 left-0 rounded-xl bg-gray w-full h-1'></div>
-											{progressBar}
+								<div className='flex flex-col items-end'>
+									<a
+										href={`tel:${user.phone}`}
+										className='text-purple underline'
+									>
+										{user.phone}
+									</a>
+									<a
+										href={`mailto:${user.email}`}
+										className='text-purple underline'
+									>
+										{user.email}
+									</a>
+								</div>
+							</div>
+							<div className='flex justify-between px-10 py-12 bg-purple-light'>
+								<div className='flex space-x-8 border-r border-gray pr-16'>
+									<div className='flex flex-col space-y-8'>
+										<p className='text-purple font-bold'>Balance</p>
+										<p className='text-purple font-bold'>Credit</p>
+										<p className='text-purple font-bold'>Purchase Progress</p>
+									</div>
+									<div className='relative flex flex-col items-end space-y-8'>
+										<p className='text-purple'>{formattedBalance}</p>
+										<p className='text-purple'>{user.credit}</p>
+										<p className='text-purple'>{purchaseProgress}</p>
+										<div className='absolute -bottom-2'>
+											<div className='relative w-14 h-1'>
+												<div className='absolute bottom-0 left-0 rounded-xl bg-gray w-full h-1'></div>
+												{progressBar}
+											</div>
 										</div>
 									</div>
 								</div>
-							</div>
-							<div className='flex flex-col justify-between pl-16'>
-								<div className='text-center text-white bg-blue rounded-md py-3.5 px-7.5'>
-									Comments
-								</div>
-								<div className='text-center text-blue bg-transparent border-blue border rounded-md py-3.5 px-7.5'>
-									Details
+								<div className='flex flex-col justify-between pl-16'>
+									<div className='text-center text-white bg-blue rounded-md py-3.5 px-7.5'>
+										Comments
+									</div>
+									<div className='text-center text-blue bg-transparent border-blue border rounded-md py-3.5 px-7.5'>
+										Details
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-				);
-			})}
+					);
+				})}
+			</div>
 		</div>
 	);
 }
 
 export async function getServerSideProps() {
-	const user = {
-		id: 'dd4e471d-18f5-413f-99a8-1cb2f8b2df52',
-		balance: '1189.44',
-		credit: 683,
-		picture: 'https://i.pravatar.cc/476',
-		name_first: 'Cheri',
-		name_last: 'Rice',
-		employer: 'Insurety',
-		email: 'cheri.rice@insurety.net',
-		phone: 7120744246,
-		address: '150 Kossuth Place, Aberdeen, Virginia, 4119',
-		comments:
-			'Id dolor cillum et qui dolor minim. Duis dolore in enim aliqua pariatur esse laboris dolore amet. Labore mollit velit occaecat officia esse cupidatat. Mollit ipsum exercitation irure mollit duis. Nisi exercitation anim tempor nulla irure. Velit sint esse enim exercitation id excepteur nulla id sunt dolore. Ex occaecat incididunt excepteur consequat ea mollit cupidatat sit enim in do cillum pariatur.',
-		created: 'Fri May 25 2018 05:57:45 GMT+0000',
-		tags: ['ex', 'amet', 'adipisicing'],
-	};
-	return { props: { users: [user] } };
+	try {
+		const { data: users } = await axios.get(`${process.env.API_URL}/api/users`);
+		return { props: { users } };
+	} catch (e) {
+		console.error(e);
+	}
+	return { props: { users: [] } };
 }
