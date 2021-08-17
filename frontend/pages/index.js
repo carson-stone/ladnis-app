67762a00dacc1a38e76bbddcbedaaac58afff0f0
Carson Stone <carson.stone123@gmail.com>
2021-08-16 23:44:24 -0400

    updated styles

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -37,47 +37,47 @@ export default function Home({ users }) {
 		console.log('progress', progress);
 		if (progress < 0.1) {
 			return (
-				<div className='absolute bottom-0 left-0 bg-green w-[0%] h-1'></div>
+				<div className='absolute bottom-0 left-0 rounded-xl bg-green w-[0%] h-1'></div>
 			);
 		} else if (0.1 <= progress && progress < 0.2) {
 			return (
-				<div className='absolute bottom-0 left-0 bg-green w-[10%] h-1'></div>
+				<div className='absolute bottom-0 left-0 rounded-xl bg-green w-[10%] h-1'></div>
 			);
 		} else if (0.2 <= progress && progress < 0.3) {
 			return (
-				<div className='absolute bottom-0 left-0 bg-green w-[20%] h-1'></div>
+				<div className='absolute bottom-0 left-0 rounded-xl bg-green w-[20%] h-1'></div>
 			);
 		} else if (0.3 <= progress && progress < 0.4) {
 			return (
-				<div className='absolute bottom-0 left-0 bg-green w-[30%] h-1'></div>
+				<div className='absolute bottom-0 left-0 rounded-xl bg-green w-[30%] h-1'></div>
 			);
 		} else if (0.4 <= progress && progress < 0.5) {
 			return (
-				<div className='absolute bottom-0 left-0 bg-green w-[40%] h-1'></div>
+				<div className='absolute bottom-0 left-0 rounded-xl bg-green w-[40%] h-1'></div>
 			);
 		} else if (0.5 <= progress && progress < 0.6) {
 			return (
-				<div className='absolute bottom-0 left-0 bg-green w-[50%] h-1'></div>
+				<div className='absolute bottom-0 left-0 rounded-xl bg-green w-[50%] h-1'></div>
 			);
 		} else if (0.6 <= progress && progress < 0.7) {
 			return (
-				<div className='absolute bottom-0 left-0 bg-green w-[60%] h-1'></div>
+				<div className='absolute bottom-0 left-0 rounded-xl bg-green w-[60%] h-1'></div>
 			);
 		} else if (0.7 <= progress && progress < 0.8) {
 			return (
-				<div className='absolute bottom-0 left-0 bg-green w-[70%] h-1'></div>
+				<div className='absolute bottom-0 left-0 rounded-xl bg-green w-[70%] h-1'></div>
 			);
 		} else if (0.8 <= progress && progress < 0.9) {
 			return (
-				<div className='absolute bottom-0 left-0 bg-green w-[80%] h-1'></div>
+				<div className='absolute bottom-0 left-0 rounded-xl bg-green w-[80%] h-1'></div>
 			);
 		} else if (0.9 <= progress && progress < 1.0) {
 			return (
-				<div className='absolute bottom-0 left-0 bg-green w-[90%] h-1'></div>
+				<div className='absolute bottom-0 left-0 rounded-xl bg-green w-[90%] h-1'></div>
 			);
 		} else if (progress === 1.0) {
 			return (
-				<div className='absolute bottom-0 left-0 bg-green w-[100%] h-1'></div>
+				<div className='absolute bottom-0 left-0 rounded-xl bg-green w-[100%] h-1'></div>
 			);
 		} else {
 			console.error('ooops');
@@ -86,7 +86,7 @@ export default function Home({ users }) {
 
 	return (
 		<div className='flex flex-col items-center justify-center min-h-screen py-2'>
-			<h1 className='text-6xl font-bold text-blue'>Landis Accounts</h1>
+			<h1 className='text-6xl font-bold text-blue mb-8'>Landis Accounts</h1>
 			{users.map((user) => {
 				const fullName = toFullName(user.name_first, user.name_last);
 				const formattedBalance = formatDollarAmount(user.balance);
@@ -97,7 +97,7 @@ export default function Home({ users }) {
 				const progressBar = getPurchaseProgressBar(user.balance, user.credit);
 
 				return (
-					<div key={user.id} className='shadow-md rounded-lg'>
+					<div key={user.id} className='min-w-[600px] shadow-md rounded-lg'>
 						<div className='flex justify-between items-center space-x-6 bg-white px-10 py-2'>
 							<div className='flex items-center space-x-7'>
 								<img
@@ -122,7 +122,7 @@ export default function Home({ users }) {
 							</div>
 						</div>
 						<div className='flex justify-between px-10 py-12 bg-purple-light'>
-							<div className='flex space-x-8 border-r border-gray-light pr-16'>
+							<div className='flex space-x-8 border-r border-gray pr-16'>
 								<div className='flex flex-col space-y-8'>
 									<p className='text-purple font-bold'>Balance</p>
 									<p className='text-purple font-bold'>Credit</p>
@@ -134,18 +134,18 @@ export default function Home({ users }) {
 									<p className='text-purple'>{purchaseProgress}</p>
 									<div className='absolute -bottom-2'>
 										<div className='relative w-14 h-1'>
-											<div className='absolute bottom-0 left-0 bg-gray w-full h-1'></div>
+											<div className='absolute bottom-0 left-0 rounded-xl bg-gray w-full h-1'></div>
 											{progressBar}
 										</div>
 									</div>
 								</div>
 							</div>
 							<div className='flex flex-col justify-between pl-16'>
-								<div className='text-white bg-blue rounded-md py-3.5 px-7.5'>
+								<div className='text-center text-white bg-blue rounded-md py-3.5 px-7.5'>
 									Comments
 								</div>
-								<div className='text-blue bg-transparent border-blue border rounded-md py-3.5 px-7.5'>
-									Comments
+								<div className='text-center text-blue bg-transparent border-blue border rounded-md py-3.5 px-7.5'>
+									Details
 								</div>
 							</div>
 						</div>
